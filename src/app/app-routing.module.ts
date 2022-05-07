4648d36a28388e0ef3a51832114413d63abbf7b2
Tyler Ruff <hello@blazed.space>
2022-05-07 17:14:46 -0400

    BLZ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevComponent } from './dev/dev.component';
 import { HomeComponent } from './home/home.component';
+import { PlacesComponent } from './places/places.component';
 import { UnknownComponent } from './unknown/unknown.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'places', component: PlacesComponent },
+  { path: 'dev', component: DevComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: UnknownComponent },
 ];
